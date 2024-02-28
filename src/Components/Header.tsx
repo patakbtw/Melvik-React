98b92d694fcde3125cda312512e14a127f00de04
Мелешкин Виктор <9580573@gmail.com>
2024-02-28 09:28:05 +0000

    header wip

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,17 @@
-import '../styles/header.scss'
+import "../styles/header.scss";
+import { menuData } from "../assets/utilities";
+import menuInterface from "../assets/utilities";
 
-export default function Header(){
-
-    return(
-        <div className="header">
-
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className="header">
+      <ul className="header__list">
+        {menuData.map((item: menuInterface, index: number) => (
+          <li key={index}>
+            <a href={item.link}>{item.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ src/assets/utilities.tsx
@@ -0,0 +1,15 @@
+const menuData = [
+    { name: 'Home', link: '/' },
+    { name: 'About', link: '/' },
+    { name: 'Services', link: '/' },
+    { name: 'Contacts', link: '/' }
+  ];
+
+interface menuInterface {
+    name: string;
+    link: string;
+}
+
+export { menuData };
+
+export default menuInterface;
